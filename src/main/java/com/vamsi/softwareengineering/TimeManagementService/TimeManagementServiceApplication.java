0c30807f368874b170a6a7df658b5a6fcba0b24c
Adabala Vamsi <vamsiavrsn@gmail.com>
2019-03-27 18:11:20 +0530

    created spring project with mongodb, lombok, and web dependencies with gradle as build tool

---
+++ src/main/java/com/vamsi/softwareengineering/TimeManagementService/TimeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vamsi.softwareengineering.TimeManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagementServiceApplication.class, args);
+	}
+
+}
